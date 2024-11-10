c3203e81279d41705c5e21c49417581210f1687b
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-10 22:34:40 +0330

    Task list page created

--- ToDoList/base/urls.py
+++ ToDoList/base/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from . import views
+from .views import taskList
+
 urlpatterns = [
-    path('', views.taskList, name='Tasks'),
+    path('', taskList.as_view(), name='Tasks'),
+    
 ]
 
--- ToDoList/base/views.py
+++ ToDoList/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def taskList(request):
-    return HttpResponse('to_do_list')
+from django.views.generic.list import ListView
+from .models import Task  
+class taskList(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'task'

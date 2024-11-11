74abeaaac71a87b505380262c96019b939dab6f0
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-11 17:48:48 +0330

    created Item created

--- ToDoList/base/urls.py
+++ ToDoList/base/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import TaskList , TaskDetailView
+from .views import TaskList , TaskDetailView , TaskCreateView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetailView.as_view(), name='task'),
+    path('create', TaskCreateView.as_view(), name='task-create'),
     
 ]
 

abb2e4dd6d42d19de68e665cb858a2b1fedc7c7b
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-12 15:42:58 +0330

    Registration updated

--- ToDoList/base/urls.py
+++ ToDoList/base/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
-from .views import TaskList , TaskDetailView , TaskCreateView , TaskUpdateView , TaskDeleteView , CustomLoginViews , CustomLogoutView
+from .views import TaskList , TaskDetailView , TaskCreateView , TaskUpdateView , TaskDeleteView , CustomLoginViews , CustomLogoutView , RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginViews.as_view(), name='login'),
-     path('logout/', CustomLogoutView.as_view(), name='logout'),
-
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
     path('create/', TaskCreateView.as_view(), name='task-create'),
